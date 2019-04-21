790e4f6dd4bf53ee9ab14f334cb81ada49425658
caglaror <caglaror@gmail.com>
2019-04-21 02:01:26 -0700

    First thoughts on project

---
+++ rcp_m1.js
@@ -0,0 +1,28 @@
+/*
+04/21/2019, Santa Clara, CA
+Recipe : rcp
+Rulez:
+- Every recipe has ingredients and/or may recipe
+- Every recipe accepts ingredients
+- An ingredient is an ingredient or ingredient and recipe
+- Recipe
+
+types---------
+ing: ingredient
+rcp: recipe
+
+
+
+// Lemonade
+rcp lemonade (ing, rcp)
+
+
+
+
+
+* */
+
+
+
+
+
